ba7900f3810c9be2d6469a7c217a89c349a6dc54
Pieter Naucke <554603@student.fontys.nl>
2025-06-10 18:07:44 +0200

    test fase5

--- Intune.ps1
+++ Intune.ps1
@@ -7,3 +7,20 @@ Invoke-WebRequest -Uri "https://raw.githubusercontent.com/pnaucke/autoupdate/ref
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/pnaucke/autoupdate/refs/heads/main/autoupdater.json" -OutFile $jsonPath
 
 PowerShell -ExecutionPolicy Bypass -File $scriptPath
+
+# Controleer of PSWindowsUpdate is geïnstalleerd
+if (-not (Get-Module -ListAvailable -Name PSWindowsUpdate)) {
+    Write-Output "PSWindowsUpdate module niet geïnstalleerd. Installeren..."
+    Install-Module -Name PSWindowsUpdate -Force
+} else {
+    Write-Output "PSWindowsUpdate module al geïnstalleerd."
+}
+
+# Controleer de Execution Policy en pas indien nodig aan
+$currentPolicy = Get-ExecutionPolicy
+if ($currentPolicy -ne "Bypass") {
+    Write-Output "Execution Policy is ingesteld op $currentPolicy. Wijzigen naar Bypass..."
+    Set-ExecutionPolicy Bypass -Force
+} else {
+    Write-Output "Execution Policy staat al op Bypass."
+}
