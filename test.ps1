f32df82624e2be09d1f2f69a85e85037b5d13d7d
Pieter Naucke <554603@student.fontys.nl>
2025-06-11 09:36:27 +0200

    test fase8

--- test.ps1
+++ test.ps1
@@ -1,2 +1,20 @@
 Add-Type -AssemblyName PresentationFramework
-[System.Windows.MessageBox]::Show("Dit is een test via Intune!")
+
+while ($true) {
+    [System.Windows.MessageBox]::Show("Dit is een test bericht van Pieter")
+
+
+    $configPath = Join-Path $PSScriptRoot "test.json"
+
+    $config = Get-Content -Path $configPath -Raw | ConvertFrom-Json
+    
+    $logFolder = $config.LogFolder
+    
+    $timestamp = Get-Date -Format "yyyy-MM-dd_HH-mm-ss"
+    $logPath = Join-Path $logFolder "update-log_$timestamp.txt"
+
+
+    "Het script is gelukt op $timestamp" | Out-File -FilePath $logPath -Encoding UTF8
+
+    Start-Sleep -Seconds 10
+}
